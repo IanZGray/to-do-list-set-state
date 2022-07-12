a26bdc4ba8b2bcbcc86109a4521de2ac3cf28fdb
IanZGray <ianzacharygray@gmail.com>
2022-07-11 20:19:02 -0400

    added function

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
 function App(){
-  const [todos, setTodos] = React.useState([
-    {
-      text: 'learn react',
-      isCompleted: false,
-    },
-    {
-      text: 'meet friend for lunch',
-      isCompleted: false,
-    },
-    {
-      text: 'build todo app',
-      isCompleted: false,
-    }        
-  ])
+  const [items,setItems] = React.useState([]);
+  const [value,setValue] = React.useState([]);  
 
-  return(
-    <>
-      {todos.map((todo, i) => <div key={i}>{todo.text}</div>)}
-    </>
+  function handleSubmit(e){
+    e.preventDefault();
+    setItems([...items,value]);
+    setValue('');
+  }
+
+  return (
+    <div className="list-big">
+      <h2>TODO</h2>
+      <TodoList items={items} />
+      <form className="list-form" onSubmit={handleSubmit}>
+        <label htmlFor="new-todo">Add todo: </label>
+        <input
+          id="new-todo"
+          value={value}
+          placeholder="Add Todo..."          
+          onChange={e => setValue(e.target.value)}
+        />
+        <button>
+          Add #{items.length + 1}
+        </button>
+      </form>
+    </div>
+  );
+}
+
+
+
+function TodoList(props){
+  return (
+    <ul className="list-added">
+      {props.items.map((item,i) => (
+        <li key={i}>{item}</li>
+      ))}
+    </ul>
   );
 }
 
 ReactDOM.render(
-  <App/>,
+  <App />,
   document.getElementById('root')
 );
